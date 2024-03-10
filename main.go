a60d194d289698bd24b00f5c0d62abb490f46005
Paolo Viotti <skypixel@gmail.com>
2024-03-10 21:29:05 +0000

    chore: refactor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
@@ -18,15 +19,28 @@ type PocketResponse struct {
 	List map[string]interface{} `json:"list"`
 }
 
+const POCKET_GET_URL = "https://getpocket.com/v3/get"
+const POCKET_GET_CONTENTTYPE = "application/json; charset=UTF-8"
+
 func main() {
 	consumerKey := os.Getenv("POCKET_CONSUMER_KEY")
 	accessToken := os.Getenv("POCKET_ACCESS_TOKEN")
 
 	if consumerKey == "" || accessToken == "" {
-		fmt.Println("Please set POCKET_CONSUMER_KEY and POCKET_ACCESS_TOKEN environment variables")
+		log.Println("Please set POCKET_CONSUMER_KEY and POCKET_ACCESS_TOKEN environment variables")
 		return
 	}
 
+	unreadCount, err := getUnreadCount(consumerKey, accessToken)
+	if err != nil {
+		log.Printf("Error getting unread count: %v\n", err)
+		return
+	}
+	fmt.Printf("Unread count: %d\n", unreadCount)
+
+}
+
+func getUnreadCount(consumerKey, accessToken string) (int, error) {
 	requestBody := PocketRequest{
 		ConsumerKey: consumerKey,
 		AccessToken: accessToken,
@@ -36,33 +50,25 @@ func main() {
 	requestBodyBytes, err := json.Marshal(requestBody)
 	if err != nil {
 		fmt.Printf("Error encoding request body: %v\n", err)
-		return
+		return 0, fmt.Errorf("error encoding request body: %v", err)
 	}
 
-	url := "https://getpocket.com/v3/get"
-
-	resp, err := http.Post(url, "application/json; charset=UTF-8", bytes.NewBuffer(requestBodyBytes))
+	resp, err := http.Post(POCKET_GET_URL, POCKET_GET_CONTENTTYPE, bytes.NewBuffer(requestBodyBytes))
 	if err != nil {
 		fmt.Printf("Error fetching data from Pocket: %v\n", err)
-		return
+		return 0, fmt.Errorf("error fetching data from Pocket: %v", err)
 	}
 	if resp.StatusCode != 200 {
 		fmt.Printf("Error fetching data from Pocket: %v\n", resp.Status)
-		return
+		return 0, fmt.Errorf("error fetching data from Pocket: %v", resp.Status)
 	}
 	defer resp.Body.Close()
-	// bodyBytes, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Printf("Error reading response body: %v\n", err)
-	// 	return
-	// }
-	// fmt.Printf("Response body: %v\n", string(bodyBytes))
 
 	var pocketResponse PocketResponse
 	if err := json.NewDecoder(resp.Body).Decode(&pocketResponse); err != nil {
 		fmt.Printf("Error decoding Pocket response: %v\n", err)
-		return
+		return 0, fmt.Errorf("error decoding Pocket response: %v", err)
 	}
 
-	fmt.Printf("Number of unread articles in Pocket: %d\n", len(pocketResponse.List))
+	return len(pocketResponse.List), nil
 }
